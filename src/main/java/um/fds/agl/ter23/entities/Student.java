84a4b9dc010646cc32c90fea270a860741b8f7ef
DrHurel <jeremyhurel34@gmail.com>
2023-09-28 10:53:10 +0200

    Update students entity to implement groups

--- src/main/java/um/fds/agl/ter23/entities/Student.java
+++ src/main/java/um/fds/agl/ter23/entities/Student.java
@@ -1,24 +1,39 @@
 package um.fds.agl.ter23.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 
 @Entity
-public class Student extends UserTER{
+public class Student extends UserTER {
 
-    // ici on mettra le groupe
+    @ManyToMany
+    private Set<Groupe> groupes;
 
-    public Student(String firstName, String lastName){
+    public Student(String firstName, String lastName) {
         super(firstName, lastName);
-        String[] roles={"ROLE_STUDENT"};
+        String[] roles = { "ROLE_STUDENT" };
         this.setRoles(roles);
+        this.groupes = new HashSet<Groupe>();
     }
 
     public Student(long id, String firstName, String lastName) {
         super(id, firstName, lastName);
-        String[] roles = {"ROLE_STUDENT"};
+        String[] roles = { "ROLE_STUDENT" };
         this.setRoles(roles);
+        this.groupes = new HashSet<Groupe>();
     }
-    public Student() {}
 
+    public Student(long id, String firstName, String lastName, Set<Groupe> groupes) {
+        super(id, firstName, lastName);
+        String[] roles = { "ROLE_STUDENT" };
+        this.setRoles(roles);
+        this.groupes = groupes;
+    }
+
+    public Student() {
+    }
 
 }
